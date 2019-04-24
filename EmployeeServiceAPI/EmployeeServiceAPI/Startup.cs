79c29f6438c744f2c424270f1b778bc8c05e4a74
Bishwanath Dey Nayan <16103022@iubat.edu>
2019-04-24 12:44:49 +0600

    API Project Added

---
+++ EmployeeServiceAPI/EmployeeServiceAPI/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(EmployeeServiceAPI.Startup))]
+
+namespace EmployeeServiceAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
